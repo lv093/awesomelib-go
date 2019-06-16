02fb03ad358d33c19cc3d1c282999955bb612e52
dongqiudi <lvliuwei@dongqiudi.com>
2019-06-16 09:41:38 +0800

    add algorithm guide

--- algorithms/sort/quick/quicksort.go
+++ algorithms/sort/quick/quicksort.go
@@ -1,1 +1,27 @@
 package quick
+
+import (
+)
+
+func partition(arr []int, p int, q int) int {
+	temp := arr[q]
+	i := p - 1
+	j := p
+	for ;j < q;j++ {
+		if arr[j] <= temp {
+			i = i + 1
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i + 1], arr[q] = arr[q], arr[i + 1]
+	return i + 1
+}
+
+func QuickSort(arr []int, low int, high int) []int {
+	if low < high {
+		middle := partition(arr, low, high)
+		QuickSort(arr, low, middle - 1)
+		QuickSort(arr, middle + 1, high)
+	}
+	return arr
+}
--- algorithms/sort/quick/quicksort_test.go
+++ algorithms/sort/quick/quicksort_test.go
@@ -1,1 +1,18 @@
 package quick
+
+import (
+	"testing"
+	"awesomelib-go/library"
+	"fmt"
+)
+
+func TestQuickSort(t *testing.T) {
+	arr := library.GenRandArray(10,1,20)
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+	QuickSort(arr, 0, len(arr) - 1)
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+}
